4c235ea699599d36a7007bdb327a1e09bdd113ba
mnalendra <muhammadnalendraz@gmail.com>
2023-11-26 23:02:51 +0700

    menambah fitur upload foto dan menambah atribut kategori

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,49 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-    <title>Home</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Home</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="style.css" rel="stylesheet">
 
 </head>
-<body>
-<div >
-<nav id="navbar-example1" class="navbar bg-body-tertiary px-3 mb-3">
-  <a style="padding-left: 5%;"  class="navbar-brand" href="#">Kedai Kopi Erky</a>
-  <ul class="nav nav-pills" style="padding-right: 10%;">
-    <li  class="nav-item">
-      <a  class="nav-link" href="#scrollspyHeading1">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#scrollspyHeading2">About</a>
-    </li>
-    <li class="nav-item item">
-      <a class="nav-link " href="#" >Menu</a>
-    </li>
-    <li class="nav-item item">
-      <a class="nav-link " href="#" >Contact</a>
-    </li>
-  </ul>
-</nav>
-</div>
 
-<section id="home">
+<body>
+  <div>
+    <nav id="navbar-example1" class="navbar bg-body-tertiary px-3 mb-3">
+      <a style="padding-left: 5%;" class="navbar-brand" href="#">Kedai Kopi Erky</a>
+      <ul class="nav nav-pills" style="padding-right: 10%;">
+        <li class="nav-item">
+          <a class="nav-link" href="#scrollspyHeading1">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#scrollspyHeading2">About</a>
+        </li>
+        <li class="nav-item item">
+          <a class="nav-link " href="#">Menu</a>
+        </li>
+        <li class="nav-item item">
+          <a class="nav-link " href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+  <section id="home">
     <div class="container-fluid">
-        <div class="row justify-content-center">
-            <div  class="col-xl-5 col-lg-6 pt-3 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                <h1 >COFFEE WISHPERS TALES IN EVERY SIP</h1>
-                <h2>"Kopi, cairan yang membangunkan, menyatu dalam aroma dan cita rasa yang memikat."</h2>
-                <div><a href="/datamenu" type="button" class="btn btn-dark btn-lg" style="margin-top: 5%;" >Buy Now</a></div>
-            </div>
-            <div class="col-xl-6 col-lg-8 order-2 order-lg-4 home-img" style="padding-left: 5%;">
-                <img src="img/home.png" class="img-fluid animated" alt="">
-            </div>
+      <div class="row justify-content-center">
+        <div class="col-xl-5 col-lg-6 pt-3 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1>COFFEE WISHPERS TALES IN EVERY SIP</h1>
+          <h2>"Kopi, cairan yang membangunkan, menyatu dalam aroma dan cita rasa yang memikat."</h2>
+          <div><a href="/datamenu" type="button" class="btn btn-dark btn-lg" style="margin-top: 5%;">Buy Now</a></div>
         </div>
+        <div class="col-xl-6 col-lg-8 order-2 order-lg-4 home-img" style="padding-left: 5%;">
+          <img src="img/home.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
     </div>
-</section>
+  </section>
 </body>
+
 </html>
