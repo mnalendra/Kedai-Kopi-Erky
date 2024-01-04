7b3bfdfebe441aaf3eda0012cead818786cb7b92
mnalendra <muhammadnalendraz@gmail.com>
2024-01-04 16:52:31 +0700

    ngubah edit menu

--- resources/views/editmenu.blade.php
+++ resources/views/editmenu.blade.php
@@ -20,14 +20,14 @@
 .container {
     width: 100%;
     display: flex;
-    max-width: 800px;
+    max-width: 850px;
     background: #fff;
     border-radius: 0px;
     box-shadow: 0 10px 15px rgba(0, 0, 0, 0.5);
 }
 
 .editmenu {
-    width: 300px;
+    width: 500px;
 }
 
 form {
@@ -60,13 +60,10 @@ p {
     margin: 10px;
 }
 
-.right{
-    width: 100%;
-    display: flex;
-    max-width: 800px;
+.right img {
+    width: 400px;
+    height: 100%;
     margin-left: 75px;
-    margin-right:0;
-    background: #000;
    
 }
 
@@ -99,18 +96,50 @@ button {
     border-radius: 5px;
 }
 
+@media (max-width: 880px) {
+    .container {
+        width: 100%;
+        max-width: 750px;
+        margin-left: 20px;
+        margin-right: 20px;
+    }
 
+    form {
+        width: 500px;
+        margin: 20px auto;
+    }
+
+    .editmenu {
+        width: 500px;
+        padding: 20px;
+    }
+
+    button {
+        width: 100%;
+    }
+
+    .right img {
+        width: 100%;
+        height: 100%;
+    }
+  }
   </style>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- Bootstrap JS and Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- SweetAlert2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
 
 <body style="background-color: burlywood">
 <div class="container">
     <div class="editmenu">
-                    <form action="/updatedata/{{ $data->id }}" method="post">
-                        <h1 class="text-center mb-4 ">Edit Data Menu</h1>
-                        <hr>
-                        <h2>change to your Happiness...</h2>
+                    <form action="/updatedata/{{ $data->id }}" method="post" enctype="multipart/form-data">
+                    <h1 class="text-center mb-4 ">Edit Data Menu</h1>
+                    <hr>
+                    <h2>change to your Happiness...</h2>
                         @csrf
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Nama Menu</label>
@@ -125,61 +154,59 @@ button {
                                 <option value="minuman">Minuman</option>
                             </select>
                         </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Harga</label>
-                            <input type="text" name="harga" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{$data->harga}}">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">{{$data->foto}}</label>
-                            <input type="file" name="foto" class="form-control" />
-                        </div>
-                        <button class="btn btn-dark" type="submit">Tambah</button>
-                    </form>
-                </div>
-
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Harga</label>
+                                <input type="text" name="harga" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{$data->harga}}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">{{$data->foto}}</label>
+                                <input type="file" name="foto" class="form-control" />
+                            </div>
+                            <div>
+                                
+                                <button class="btn btn-dark" type="submit">Tambah</button>
+                            </div>
         <div class="right">
-            
-       
-            
+            <img src="img/pic1.jpg"alt="">
         </div>
-    
+                    </form>
+   
+                    </div>
 </div>
-  <!-- Bootstrap JS and Popper.js -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <!-- SweetAlert2 JS -->
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            // Menangkap formulir
-            var form = document.querySelector("form");
 
-            // Menambahkan event listener ke formulir
-            form.addEventListener("submit", function(event) {
-                event.preventDefault(); // Menghentikan perilaku default formulir
 
-                // Menampilkan alert
-                Swal.fire({
-                    title: "Do you want to save the changes?",
-                    showDenyButton: true,
-                    showCancelButton: true,
-                    confirmButtonText: "Save",
-                    denyButtonText: `Don't save`
-                }).then((result) => {
-                    /* Read more about isConfirmed, isDenied below */
-                    if (result.isConfirmed) {
-                        Swal.fire("Saved!", "", "success");
-                    } else if (result.isDenied) {
-                        Swal.fire("Changes are not saved", "", "info");
-                    }
-                });
+</div>
 
-                // Lanjutkan dengan pengiriman formulir ke server jika diperlukan
-                form.submit();
+  <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        // Menangkap formulir
+        var form = document.querySelector("form");
+
+        // Menambahkan event listener ke formulir
+        form.addEventListener("submit", function(event) {
+            event.preventDefault(); // Menghentikan perilaku default formulir
+
+            // Menampilkan alert
+            Swal.fire({
+                title: "Do you want to save the changes?",
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: "Save",
+                denyButtonText: `Don't save`
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    // Lanjutkan dengan pengiriman formulir ke server jika diperlukan
+                    form.submit();
+                    Swal.fire("Saved!", "", "success");
+                } else if (result.isDenied) {
+                    Swal.fire("Changes are not saved", "", "info");
+                }
             });
         });
-    </script>
+    });
+</script>
+
 
 </body>
 
