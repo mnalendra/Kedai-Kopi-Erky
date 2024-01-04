5f4214078ab6dd7bb43e4566ca3dec379a17e9ce
helmiyaaz <helmiya76@gmail.com>
2024-01-04 19:07:40 +0700

    mengubah login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -44,10 +44,15 @@
                     @enderror
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1">   
                 </div>
-                <button type="submit" class="btn btn-dark">Login</button>
+                <div class="form-check d-flex align-items-center">
+                    <input class="form-check-input ms-2" type="checkbox" id="showPassword" onchange="togglePassword()">
+                    <label class="form-check-label" for="showPassword">Show Password</label>
+                </div>
+                <button type="submit" class="btn btn-dark mt-3">Login</button>
             </form>
+             <p class="mt-3" style="color: brown; text-align: center">Don't have an account? <a href="/register">Register here</a></p>
             </div>
           </div>
         </div>
@@ -57,34 +62,18 @@
     
   </div>
 
-    
-    <!-- <div class="container">
-        <div class="row">
-            <div class="card">
-                <div class="card-body">
-                    <form action="/loginuser" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Login</button>
-                    </form>
-
-                </div>
-            </div>
-        </div>
-    </div> -->
     </section>
+
+    <script>
+        function togglePassword() {
+            const passwordField = document.getElementById('exampleInputPassword1');
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+            } else {
+                passwordField.type = 'password';
+            }
+        }
+    </script>
 </body>
 
 </html>
